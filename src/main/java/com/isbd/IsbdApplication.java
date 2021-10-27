2d1c933ef7a0b9345990291bbcca9e4c75643e3a
Vladislav <i.96petrov@gmail.com>
2021-10-27 16:56:51 +0300

    initial commit

---
+++ src/main/java/com/isbd/IsbdApplication.java
@@ -0,0 +1,13 @@
+package com.isbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsbdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsbdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isbd/IsbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsbdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
