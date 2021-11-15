b9dc200d9fd1f2f30c0465f191560b22e7494370
Vladislav <i.96petrov@gmail.com>
2021-11-15 22:04:33 +0300

    bug fix

--- src/main/java/com/isbd/model/AppliedEffect.java
+++ src/main/java/com/isbd/model/AppliedEffect.java
@@ -1,11 +1,13 @@
 package com.isbd.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 public class AppliedEffect extends Effect {
     private long playerId;
