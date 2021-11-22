cdac9069e3de2ddc03c06d0917b02751bb6e2ffc
Vladislav <i.96petrov@gmail.com>
2021-11-22 12:17:11 +0300

    translation of error messages 2

--- src/main/java/com/isbd/dto/UserDto.java
+++ src/main/java/com/isbd/dto/UserDto.java
@@ -6,9 +6,9 @@ import javax.validation.constraints.Size;
 
 @Data
 public class UserDto {
-    @Size(min = 4, message = "Username must contain at least 4 symbols")
+    @Size(min = 4, message = "Имя пользователя должно содержать как минимум 4 символа")
     private final String username;
 
-    @Size(min = 8, message = "Password must contain at least 8 symbols")
+    @Size(min = 8, message = "Пароль должн содержать как минимум 8 символов")
     private final String password;
 }
