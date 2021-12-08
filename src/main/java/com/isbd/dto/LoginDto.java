83d9f835f5151fa511f4384a3e3b0513c14e3b0c
Vladislav <i.96petrov@gmail.com>
2021-12-08 23:10:08 +0300

    grammar error

--- src/main/java/com/isbd/dto/LoginDto.java
+++ src/main/java/com/isbd/dto/LoginDto.java
@@ -9,6 +9,6 @@ public class LoginDto {
     @Size(min = 4, message = "Имя пользователя должно содержать как минимум 4 символа")
     private final String username;
 
-    @Size(min = 8, message = "Пароль должн содержать как минимум 8 символов")
+    @Size(min = 8, message = "Пароль должен содержать как минимум 8 символов")
     private final String password;
 }
