1a0748ad79dbc31dedb990a8249964ea50a602b9
Vladislav <i.96petrov@gmail.com>
2021-12-09 20:14:12 +0300

    bug fix

--- src/main/java/com/isbd/security/AuthenticationFacadeImpl.java
+++ src/main/java/com/isbd/security/AuthenticationFacadeImpl.java
@@ -2,13 +2,18 @@ package com.isbd.security;
 
 import com.isbd.exception.WrongCredentialsException;
 import com.isbd.security.jwt.JwtUser;
+import com.isbd.service.AuthService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class AuthenticationFacadeImpl implements AuthenticationFacade {
+    private final AuthService authService;
+
     @Override
     public Authentication getAuthentication() {
         return SecurityContextHolder.getContext().getAuthentication();
@@ -19,7 +24,9 @@ public class AuthenticationFacadeImpl implements AuthenticationFacade {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (!(auth instanceof AnonymousAuthenticationToken)) {
             JwtUser jwtUser = (JwtUser) auth.getPrincipal();
-            return jwtUser.getId();
+            if (authService.isPlayerExists(jwtUser.getId())) {
+                return jwtUser.getId();
+            }
         }
         throw new WrongCredentialsException("У вас не прав на просмотр данной информации");
     }
