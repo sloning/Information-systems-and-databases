3c5ff45329d86c37a2b501a4c47809b9f69a6d31
Vladislav <i.96petrov@gmail.com>
2021-12-20 19:16:13 +0300

    obtained kit all args constructor and no args constructor

--- src/main/java/com/isbd/model/ObtainedKit.java
+++ src/main/java/com/isbd/model/ObtainedKit.java
@@ -4,9 +4,18 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
 public class ObtainedKit extends Kit {
     private LocalDateTime lastObtained;
+
+    public ObtainedKit() {
+    }
+
+    public ObtainedKit(int id, String name, int reload, List<InventoryItem> items, LocalDateTime lastObtained) {
+        super(id, name, reload, items);
+        this.lastObtained = lastObtained;
+    }
 }
